c6572aedbf540683f5edb5f2da44af6c837322c3
JasonRoberts11 <33434307+JasonRoberts11@users.noreply.github.com>
2018-08-22 17:52:10 -0600

    funny doings

--- scripts/fun.js
+++ scripts/fun.js
@@ -1,17 +1,22 @@
 /**
 javascript:var%20i,s,ss=['http://jasonroberts11.github.io/scripts/fun.js'];for(i=0;i!=ss.length;i++){s=document.createElement('script');s.src=ss[i];document.body.appendChild(s);}void(0);
 **/
-var b = document.getElementsByTagName("html");
-            var c = b[0].innerHTML;
+
+
+
+	var b = document.getElementsByTagName("html");
+       	    var c = b[0].innerHTML;
             var d = ""
             var e =0;
+		g = prompt("Char to Replace", " ");
+		h = prompt("Replace with", ".....");
             for(var i =0; i<c.length;i++){
                 if(e==0){
                     if(c[i]=="<"){
                         e=1;
                     }
-                    if(c[i]==" "){
-                        d+=".....";
+                    if(c[i]==g){
+                        d+=h;
                     }else{
                        d+=c[i];
                     }
