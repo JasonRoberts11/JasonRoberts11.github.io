22444e47ba75404df0acf086b35c199b95ce9b37
JasonRoberts11 <33434307+JasonRoberts11@users.noreply.github.com>
2018-08-23 07:23:19 -0600

    added an s

--- scripts/fun.js
+++ scripts/fun.js
@@ -1,5 +1,5 @@
 /**
-javascript:var%20i,s,ss=['http://jasonroberts11.github.io/scripts/fun.js'];for(i=0;i!=ss.length;i++){s=document.createElement('script');s.src=ss[i];document.body.appendChild(s);}void(0);
+javascript:var%20i,s,ss=['https://jasonroberts11.github.io/scripts/fun.js'];for(i=0;i!=ss.length;i++){s=document.createElement('script');s.src=ss[i];document.body.appendChild(s);}void(0);
 **/
 
 
