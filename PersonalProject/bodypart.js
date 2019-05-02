01f13b056fdaddd7aad1b329123dbb6b33796e6f
JasonRoberts11 <33434307+JasonRoberts11@users.noreply.github.com>
2019-05-01 21:12:06 -0600

    finisheed

--- PersonalProject/bodypart.js
+++ PersonalProject/bodypart.js
@@ -1,65 +1,71 @@
-function BodyPart(name="unnamed",typ=0,color="#FFAA00"){
+function BodyPart(name="unnamed",typ=0,col="#FFAA00",num){
 	//This is the object BodyPart, name is what the image is called, and typ is which version of that body part is shown.
-	//the color variable represents the color of the image.
-
-    this.color=color;
+	//the color variable represents the color change of the image.
+  this.color=col;
 	this.name=name;
 	this.typ=typ;
-	this.img = new Image();
-	//this.img.crossOrigin = "Anonymous";
+	this.img;
+  this.images=[];
+  for(var i=0;i<num;i++){
+    this.images[i]=new Image();
+    this.images[i].src= "images/"+this.name+"_"+i+".png";
+  }
+  this.convertedimg;
 
-	//this.img.
-	//Draws the object to the canvas;
-	this.setImage=function(){
-		this.img.src = "images/"+this.name+"_"+this.typ+".png";
+	//Loads the image from file to the object;
+	this.loadImage=function(){
+		this.img=this.images[this.typ];
+    this.convertedimg=this.convertImageColor();
 	}
+  //Draws the image of the body part onto the main canvas
 	this.draw = function(){
-		
-		ctx.drawImage(this.convertImageColor(),0,0);
-		//console.log(this.img.width);
+		ctx.drawImage(this.convertedimg,0,0,this.img.width*scalefactor,this.img.height*scalefactor);
 	}
-	
+	//Converts the original image to the colored image, returns the new image as a canvas
 	this.convertImageColor=function(){
+		//Converts the color string to rgb value
+		var rgbcolor = convertColorToRGB(this.color);
+
 		//Creates temporary canvas
 		var can = document.createElement("canvas");
 		var cw=this.img.width;
 		var ch=this.img.height;
 		can.width=cw;
 		can.height=ch;
-        
-        
 		var etx = can.getContext("2d");
-		//Draws image onto canvas
-		
-        etx.drawImage(this.img,0,0);
-		//etx.fillRect(5,10,50,50);
-		//Converts the color string to rgb value
-		var rgbcolor = this.convertColorToRGB();
-        console.log("rgb"+rgbcolor)
+
+		//Draws image onto the temporary canvas
+    etx.drawImage(this.img,0,0);
+
 		//Gets the data from the canvas
 		var idata=etx.getImageData(0,0,cw,ch);
-		for(var i =0;i<cw*ch*4;i+=4){
+
+   //Iterate through each pixel in the image, and gets rgb components
+   	for(var i =0;i<cw*ch*4;i+=4){
 			var r = idata.data[i];
 			var g = idata.data[i+1];
 			var b = idata.data[i+2];
-			var a = idata.data[i+3];
-			if(true){
-				if(true){
-					idata.data[i]=rgbcolor[0];
-					idata.data[i+1]=rgbcolor[1];
-					idata.data[i+2]=rgbcolor[2];
-				}
+      //Calcuates the brightness of each pixel to keep the brightness after the conversion.
+      var bright = (r+g+b);
+      if(bright>255)bright=255;
+      bright/=256;
+      //Sets the pixel color to the desired color if it isn't black
+			if(!(r<20&&g<20&&b<20)){
+				idata.data[i]=rgbcolor[0]*bright;
+				idata.data[i+1]=rgbcolor[1]*bright;
+				idata.data[i+2]=rgbcolor[2]*bright;
 			}
 		}
-        etx.getImageData(0,0,cw,ch).data=(data);
+    //Sets the image data to the converted data;
+    etx.putImageData(idata,0,0);
 		return can;
-	}
-	
-	this.convertColorToRGB=function(){
-		var c = this.color;
+	}	
+}
+
+//converts the string "#XXXXXX" to an array containing the rgb
+convertColorToRGB=function(c="#000000"){
 		var r = parseInt((c[1]+c[2]),16);
 		var g = parseInt((c[3]+c[4]),16);
 		var b = parseInt((c[5]+c[6]),16);
 		return [r,g,b];
 	}
-}
--- PersonalProject/main.js
+++ PersonalProject/main.js
@@ -1,29 +1,155 @@
 //Initial Variables
 var width=768;
 var height=768;
+var scalefactor=0.5;
 
+//An array with the names of the image files, and how many different types there are
+var partNames = ["body,1","head,3","pants,4","shirt,4","hair,3","eyes,5","nose,2","mouth,3","shoes,2"];
+
+//An array that random colors are chosen from
+var colors = ["#666666","#6666FF","#00FFFF","#66FF66","#FFFF00","#FFFFFF","#FF6666","#FF00FF"];
 
 //Defines the variables for the Program
 var main_div = document.getElementById("main");
 
 //Enables the ability to draw on a canvas.
 var canvas = document.createElement("canvas");
-canvas.width=width;
-canvas.height=height;
+canvas.width=width*scalefactor;
+canvas.height=height*scalefactor;
 var ctx = canvas.getContext("2d");
+
 //Adds the canvas to the div
 main_div.appendChild(canvas);
 
+//Creates an array of body parts based on text;
+var partArray = [];
+for(var i = 0; i < partNames.length ; i++){
+  var a = partNames[i].split(',');
+  partArray.push(new BodyPart(a[0],0,"#FFFFFF",a[1]));  
+}
+Randomize();
+
+//Creates the Table of Buttons to Change the Style
+//Row: < , Name , > , Color
+var colorpickers=[];
+colorpickers.push(null);
+var table = document.createElement("table");
+main_div.appendChild(table);
+for(var i=1;i<partArray.length;i++){
+  var row = document.createElement("tr");
+  table.appendChild(row);
+
+  var leftbutton = document.createElement("button");
+  leftbutton.innerHTML="<";
+  addElementToRow(leftbutton,row);
+
+  addHandler("click",leftbutton,ChangeBody,i,-1);
+
+  
+  var label = document.createElement("td");
+  label.innerHTML=Capitalize(partArray[i].name);
+  row.appendChild(label);
 
 
-//Creates the body parts
-var body = new BodyPart("body",0,0);
-setInterval(draw,100);
-//
-function draw(){
-	console.log("start draw");
-	body.setImage();
+  var rightbutton = document.createElement("button");
+  rightbutton.innerHTML=">";
+  addElementToRow(rightbutton,row);
+  addHandler("click",rightbutton,ChangeBody,i,1);
+
+  var colorpicker = document.createElement("input");
+  addElementToRow(colorpicker,row);
+  colorpicker.type="color";
+  colorpicker.value=partArray[i].color;
+  addHandler("change",colorpicker,ChangeColor,i,0);
+  colorpickers.push(colorpicker);
+}
+
+//This function handles the button clicks.
+function addHandler(event,element,func,i,d){
+  element.addEventListener(event,function(e){
+    func(i,d);
+  });
+}
+
+//This function adds an element to a desired row in the table;
+function addElementToRow(element,row){
+  var td = document.createElement("td");
+  row.appendChild(td);
+  td.appendChild(element);
+}
+
+//This function Capitalizes the words that show up on the table
+function Capitalize(str){
+  return str.charAt(0).toUpperCase() + str.slice(1);
+}
+
+//This function changes a body part with index i, by delta d.
+function ChangeBody(i,d){
+  var t = partArray[i].typ;
+  var l = partNames[i].split(',')[1];
+  t+=d;
+  if(t<0)t=l-1;
+  if(t>=l)t=0;
+  partArray[i].typ=t;
+  partArray[i].loadImage();
+  draw();
+}
+//this function changes the color of the part to the colorpicker value;
+function ChangeColor(i,c){
+  partArray[i].color=colorpickers[i].value;
+  if(i==1){
+    partArray[0].color=colorpickers[i].value;
+    partArray[0].loadImage();
+  }
+  partArray[i].loadImage();
+  draw();
+}
+
+//Function that selects the BodyPart object in the partarray array that has a certain name
+function selectPartByName(name){
+  for(var i=0;i<partArray.length;i++){
+    if(partArray[i].name==name){
+      return partArray[i];
+    }
+  }
+  return null;
+}
+
+
+//Function that randomly picks body parts;
+function Randomize(){
+  for(var i = 0; i < partArray.length ; i++){
+    var a = partNames[i].split(',');
+    var b = Math.floor(Math.random()*a[1]);
+    var c = Math.floor(Math.random()*colors.length);
+    partArray[i].typ=b;
+    partArray[i].color=colors[c];
+  }
+  //Sets the head color to the body color
+  selectPartByName("head").color=selectPartByName("body").color;
+  //Sets the Eyes to white
+  selectPartByName("eyes").color="#FFFFFF";
+  //Sets the inner mouth to red;
+  selectPartByName("mouth").color="#FF0000";
+}
+
+
+
+//Loads the images after 2 seconds
+setTimeout(setImages,2000);
+setTimeout(draw,2100);
+
+
+function setImages(){
+  for(var i=0;i<partArray.length;i++){
+    partArray[i].loadImage();
+  }
+}
+
+function draw(){	
 	ctx.fillStyle="#EFEFEF"
 	ctx.fillRect(0,0,width,height);
-	body.draw();
+  for(var i=0;i<partArray.length;i++){
+    partArray[i].draw();
+  }
 }
